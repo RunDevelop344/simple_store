b8e9dbcbee8e3f370a1f40d6ac125bdf9c142263
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-01 23:56:46 -0600

    updated category and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
   validates :stock_quantity, presence: true
+
+  #Association with category
+  belongs_to :category
 end
