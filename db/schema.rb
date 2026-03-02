4c1b731d5a4996a356c55724f6b0a047fbb5d043
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-01 23:33:15 -0600

    database migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_02_021714) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_02_052950) do
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
